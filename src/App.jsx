e2df842342cc98ce54fde924a2361e49a5580e83
gregory6z <gregoryrag@gmail.com>
2022-07-11 13:01:58 +0200

    create global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./global.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gregory Praxedes"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.css";
+
+export function Header() {
+  return <strong className={styles.header}>Ignite Feed</strong>;
+}
