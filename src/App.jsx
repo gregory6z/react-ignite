accaa90d51137250fd6cf4897d990efab3fb994c
gregory6z <gregoryrag@gmail.com>
2022-07-11 12:36:19 +0200

    Property concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Post } from "./Post";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Post
+        author="Gregory Praxedes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
+      />
+      <Post
+        author="Valesca Vieira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
+      />
+    </div>
+  );
 }
 
 export default App;
