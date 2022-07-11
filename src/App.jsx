a37a6ccd6651cb261fdcb04247ddf278699c5bbe
gregory6z <gregoryrag@gmail.com>
2022-07-11 16:36:05 +0200

    create Post and sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+
+import styles from "./App.module.css";
 
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Gregory Praxedes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
-      />
-      <Post
-        author="Valesca Vieira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {" "}
+          <Post
+            author="Gregory Praxedes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
+          />
+          <Post
+            author="Valesca Vieira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
+          />
+        </main>
+      </div>
     </div>
   );
 }
