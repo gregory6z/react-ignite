64267013822a5038091bd1b444ae0faa515a7b09
gregory6z <gregoryrag@gmail.com>
2022-07-12 13:12:32 +0200

    create comment functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,65 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
-
+import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 function App() {
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: "https://github.com/gregory6z.png",
+        name: "Gregory Praxedes",
+        role: "Developer Web",
+      },
+      content: [
+        { type: "paragraph", content: "Fala galeraa 👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2022-06-03 20:00:00"),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: "https://github.com/diego3g.png",
+        name: "Diego Fernandes",
+        role: "Educator @Rocketseat",
+      },
+      content: [
+        { type: "paragraph", content: "Fala galeraa 👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2022-06-10 20:00:00"),
+    },
+  ];
+
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {" "}
-          <Post
-            author="Gregory Praxedes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
-          />
-          <Post
-            author="Valesca Vieira"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam maiores aliquam quis, dolorem fugiat quasi libero quia facere quisquam dicta laudantium. Culpa magnam alias eos repellat et dolores laudantium? Maxime."
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,44 +1,84 @@
 import styles from "./Post.module.css";
 
+import { format, formatDistanceToNow } from "date-fns";
+import fr from "date-fns/locale/fr";
+
+import { useState } from "react";
+
 import igniteLogo from "../assets/ignite-logo.svg";
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState(["Post Muito Bacana"]);
+
+  const [newCommentText, setNewCommentText] = useState("");
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'à' HH:mm'h'",
+    {
+      locale: fr,
+    }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: fr,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    
+    setNewCommentText("");
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/gregory6z.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Gregory Praxedes</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="11 mai à 08h13" dateTime="2022-5-11 08:13:30">
-          posté il y a 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="#"> jane.design/doctorcare</a>
-        </p>
-        <p>
-          {" "}
-          <a href="#">#novoprojeto</a>
-          <a href="#"> #rocketseat</a>
-          <a href="#">#nlw</a>{" "}
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                {" "}
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Laissez votre feedback</strong>
 
-        <textarea placeholder="laissez un commentaire" />
+        <textarea
+          placeholder="laissez un commentaire"
+          name="comment"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
         <footer>
           <button type="submit">Commentaire</button>
         </footer>
@@ -46,9 +86,9 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment content={comment} />;
+        })}
       </div>
     </article>
   );
